7b51ad402eaa70102c213bdf2cba913a6005ba11
A7024E <rudqls6671@naver.com>
2025-10-30 07:31:06 +0900

    test: 1부터 45까지 범위인 중복되지 않은 숫자를 생성하는지 테스트

--- src/test/java/lotto/model/lottonumberstrategy/LottoNumberStrategyTest.java
+++ src/test/java/lotto/model/lottonumberstrategy/LottoNumberStrategyTest.java
@@ -4,6 +4,8 @@ package lotto.model.lottonumberstrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,18 @@ class LottoNumberStrategyTest {
         List<Integer> numbers = randomNumberStrategy.generateNumbers();
         // then
         assertThat(numbers).allMatch(number -> number >=1 && number <=45);
+    }
+
+    @DisplayName("1부터 45까지 중복되지않은 랜덤한 숫자를 생성한다")
+    @Test
+    void nonDuplicateNumbers(){
+        // given
+        RandomNumberStrategy randomNumberStrategy = new LottoNumberStrategy();
+        // when
+        List<Integer> numbers = randomNumberStrategy.generateNumbers();
+        // then
+        HashSet<Integer> numberSet = new HashSet<>(numbers);
+        assertThat(numbers.size()).isEqualTo(numberSet.size()).isBetween(1,45);
     }
 
 }
