57760b62fdd3cd0e2f86e0460ab7ea90f2a55de4
A7024E <rudqls6671@naver.com>
2025-10-29 11:12:39 +0900

    feat(ErrorMessages): 예외상황 메시지들을 관리하는 enum 생성

---
+++ src/main/java/lotto/exception/ErrorMessages.java
@@ -0,0 +1,20 @@
+package lotto.exception;
+
+public enum ErrorMessages {
+
+    ERROR_PREFIX("[ERROR]"),
+    INVALID_EMPTY_INPUT("빈 값을 입력할 수 없습니다. 다시 입력해 주세요."),
+    INVALID_ONLY_NUMERIC("문자를 입력할 수 없습니다. 숫자만 입력해 주세요."),
+    INVALID_CONTAINS_WHITESPACE("공백을 입력할 수 없습니다. 공백을 제외하고 다시 입력해 주세요.");
+
+
+    private final String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_PREFIX.message+this.message;
+    }
+}
