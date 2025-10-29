3486f4f6eb2baa63c5e2d4b14e20c71bb9ea70db
A7024E <rudqls6671@naver.com>
2025-10-29 11:21:54 +0900

    refactor(ErrorMessages): 숫자입력에 관한 메시지 변경

--- src/main/java/lotto/exception/ErrorMessages.java
+++ src/main/java/lotto/exception/ErrorMessages.java
@@ -4,7 +4,7 @@ public enum ErrorMessages {
 
     ERROR_PREFIX("[ERROR]"),
     INVALID_EMPTY_INPUT("빈 값을 입력할 수 없습니다. 다시 입력해 주세요."),
-    INVALID_ONLY_NUMERIC("문자를 입력할 수 없습니다. 숫자만 입력해 주세요."),
+    INVALID_ONLY_NUMERIC("정수만 입력 가능합니다 다시 입력해 주세요."),
     INVALID_CONTAINS_WHITESPACE("공백을 입력할 수 없습니다. 공백을 제외하고 다시 입력해 주세요.");
 
 
