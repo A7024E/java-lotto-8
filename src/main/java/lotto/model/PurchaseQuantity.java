8f923c02df75de62b5c9c55c14cb3c7fe4704dc4
A7024E <rudqls6671@naver.com>
2025-10-29 12:50:02 +0900

    feat: 구입 금액을 1000원 단위로 나누어 수량을 계산하는 기능 추가
    - 구입금액에 대한 유효성 검사를 하여 구입금액을 생성한다.
    - 구입 금액에 따른 수량을 계산한다.

--- src/main/java/lotto/model/PurchaseQuantity.java
+++ src/main/java/lotto/model/PurchaseQuantity.java
@@ -1,9 +1,12 @@
 package lotto.model;
 
+import lotto.exception.ErrorMessages;
+
 public class PurchaseQuantity {
     private int quantity;
 
     private PurchaseQuantity(int quantity) {
+        validate(quantity);
         this.quantity = quantity;
     }
 
@@ -11,4 +14,16 @@ public class PurchaseQuantity {
         return new PurchaseQuantity(quantity);
     }
 
+    public int calculateQuantity() {
+        return quantity / 1000;
+    }
+
+    private void validate(int quantity) {
+        if(quantity / 1000 != 0) {
+            throw new IllegalArgumentException(ErrorMessages.INVALID_NOT_DIVIDE_PURCHASE_AMOUNT.getMessage());
+        }
+        if(quantity < 1000){
+            throw new IllegalArgumentException(ErrorMessages.INVALID_PURCHASE_AMOUNT_MINIMUM.getMessage());
+        }
+    }
 }
