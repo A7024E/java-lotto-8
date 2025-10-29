4b1424a26cfdf1b0933a4b6d029c560f1b7bbe48
A7024E <rudqls6671@naver.com>
2025-10-30 05:49:12 +0900

    test: 주어진 수량에 따라 로또를 발행한다

---
+++ src/test/java/lotto/model/LottoGeneratorTest.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoGeneratorTest {
+    @DisplayName("주어진 수량대로 로또를 발행한다")
+    @Test
+    void issueLottos() {
+        // given
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int quantity = 5;
+        LottoGenerator lottoGenerator = LottoGenerator.from(() -> lottoNumbers);
+        // when
+        List<Lotto> lottos = lottoGenerator.issueLottos(quantity);
+        // then
+        List<Lotto> compareLottos = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 6))
+        );
+
+        assertThat(lottos).hasSize(5).isEqualTo(compareLottos);
+
+    }
+
+}
