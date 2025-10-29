56210ac4f1867fc742b605d8a579180013e469a0
A7024E <rudqls6671@naver.com>
2025-10-30 05:39:03 +0900

    feat(LottoNumberStrategy): 로또 번호 오름차순으로 정렬하는 기능 추가

--- src/main/java/lotto/model/lottonumberstrategy/LottoNumberStrategy.java
+++ src/main/java/lotto/model/lottonumberstrategy/LottoNumberStrategy.java
@@ -1,12 +1,25 @@
 package lotto.model.lottonumberstrategy;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberStrategy implements RandomNumberStrategy {
 
     @Override
     public List<Integer> generateNumbers() {
+        List<Integer> numbers = generateUniqueNumbers();
+        return sort(numbers);
+    }
+
+    private List<Integer> generateUniqueNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    private List<Integer> sort(List<Integer> numbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
     }
 }
