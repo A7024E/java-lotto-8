fdca2ad6d4235b02c7e8dcc2a20da4beea234e29
A7024E <rudqls6671@naver.com>
2025-10-29 10:46:29 +0900

    feat: 로또 구입금액을 입력받는 기능 추가

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,19 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    InputView inputView;
+    OutputView outputView;
+
+    public LottoController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void run(){
+        outputView.printPurchaseAmountMessage();
+        inputView.input();
+    }
+}
