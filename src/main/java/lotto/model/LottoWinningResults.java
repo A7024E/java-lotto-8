bf177943329493b470df726032c6d802bd5fdd4d
A7024E <rudqls6671@naver.com>
2025-10-31 03:58:48 +0900

    feat: 총 수익률을 계산하는 기능 추가

--- src/main/java/lotto/model/LottoWinningResults.java
+++ src/main/java/lotto/model/LottoWinningResults.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class LottoWinningResults {
     private final Map<LottoRank,Integer> lottoRankResults;
@@ -11,6 +12,16 @@ public class LottoWinningResults {
 
     public static LottoWinningResults from(Map<LottoRank, Integer> lottoRankResults) {
         return new LottoWinningResults(lottoRankResults);
+    }
+
+    public double calculateTotalWinningPrice(int purchaseAmount) {
+        long totalWinningPrice = 0;
+        for (Entry<LottoRank, Integer> lottoRankIntegerEntry : lottoRankResults.entrySet()) {
+            LottoRank rank = lottoRankIntegerEntry.getKey();
+            int count = lottoRankIntegerEntry.getValue();
+            totalWinningPrice+= rank.getWinningAmount() * count;
+        }
+        return (double) totalWinningPrice / purchaseAmount * 100;
     }
 
     public Map<LottoRank, Integer> getLottoRankResults() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
 import lotto.model.Lotto;
+import lotto.model.LottoRank;
 
 public class OutputView {
 
@@ -27,6 +30,38 @@ public class OutputView {
 
     public void printBonusMessage() {
         System.out.println(LINE_SEPARATOR + "보너스 번호를 입력해 주세요.");
+    }
+
+    public void printWinningResultMessage() {
+        System.out.println(LINE_SEPARATOR + "당첨 통계" + LINE_SEPARATOR + "---");
+    }
+
+    public void printLottoWinningResult(Map<LottoRank, Integer> winningResult) {
+        StringJoiner formatResult = formatWinningResult(winningResult);
+        System.out.println(formatResult);
+    }
+
+    public void printRateOrReturn(double rateOfReturn) {
+        String formatRateOfReturn = String.format("%,.1f", rateOfReturn);
+        System.out.println("총 수익률은 " + formatRateOfReturn + "%입니다.");
+    }
+
+    private StringJoiner formatWinningResult(Map<LottoRank, Integer> winningResult) {
+        StringJoiner sj = new StringJoiner(LINE_SEPARATOR);
+        for (LottoRank lottoRank : LottoRank.values()) {
+            if (isWinningAmount(lottoRank)) {
+                appendWinningResult(winningResult, lottoRank, sj);
+            }
+        }
+        return sj;
+    }
+
+    private boolean isWinningAmount(LottoRank lottoRank) {
+        return lottoRank.getWinningAmount() > 0L;
+    }
+
+    private void appendWinningResult(Map<LottoRank, Integer> winningResult, LottoRank lottoRank, StringJoiner sj) {
+        sj.add(lottoRank.getDescription() + " - " + winningResult.getOrDefault(lottoRank, 0) + "개");
     }
 
     public void printErrorMessage(String exceptionMessage) {
