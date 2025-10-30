8ff05c4041593500625cb1823bd1cf8af08f51a2
A7024E <rudqls6671@naver.com>
2025-10-30 13:00:58 +0900

    refactor: 입력 유효성 로직 수정
    - 각각의 유효성 검사를 담당할 수 있도록 메서드를 분리하였음.

--- src/main/java/lotto/exception/io/InputValidation.java
+++ src/main/java/lotto/exception/io/InputValidation.java
@@ -4,15 +4,27 @@ import lotto.exception.ErrorMessages;
 
 public class InputValidation {
 
-    public static void validate(String input){
-        if(isNullOrEmpty(input)){
+    public static void validateNullOrEmpty(String input) {
+        if (isNullOrEmpty(input)) {
             throw new IllegalArgumentException(ErrorMessages.INVALID_EMPTY_INPUT.getMessage());
         }
-        if(isBlankContains(input)){
+    }
+
+    public static void validateBlankContains(String input) {
+        if (isBlankContains(input)) {
             throw new IllegalArgumentException(ErrorMessages.INVALID_CONTAINS_WHITESPACE.getMessage());
         }
-        if(isNumeric(input)){
+    }
+
+    public static void validateNumeric(String input) {
+        if (isNumeric(input)) {
             throw new IllegalArgumentException(ErrorMessages.INVALID_ONLY_NUMERIC.getMessage());
+        }
+    }
+
+    public static void validateDelimiter(String input) {
+        if (!input.matches("^-?\\d+(,-?\\d+)*$")) {
+            throw new IllegalArgumentException("당첨 번호는 공백없이 쉼표로 구분된 정수들로 입력해야 합니다.");
         }
     }
 
--- src/test/java/lotto/exception/io/InputValidationTest.java
+++ src/test/java/lotto/exception/io/InputValidationTest.java
@@ -17,7 +17,7 @@ class InputValidationTest {
     @NullAndEmptySource
     void inputIsNullOrEmpty(String input) {
         // given//when// then
-        assertThatThrownBy(()->InputValidation.validate(input))
+        assertThatThrownBy(()->InputValidation.validateNullOrEmpty(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessages.INVALID_EMPTY_INPUT.getMessage());
     }
@@ -27,29 +27,53 @@ class InputValidationTest {
     @ValueSource(strings = {" ","1000 ","1 000"," 1000","1000 "})
     void inputIsWhitespace(String input) {
         // given//when// then
-        assertThatThrownBy(()->InputValidation.validate(input))
+        assertThatThrownBy(()->InputValidation.validateBlankContains(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessages.INVALID_CONTAINS_WHITESPACE.getMessage());
     }
 
-    @DisplayName("입력값에 공백이 들어가면 예외를 발생한다")
+    @DisplayName("입력값에 문자열이 들어가면 예외를 발생한다")
     @ParameterizedTest
     @ValueSource(strings = {"a","-","1000원","100@","1000a"})
     void inputIsNumeric(String input) {
         // given//when// then
-        assertThatThrownBy(()->InputValidation.validate(input))
+        assertThatThrownBy(()->InputValidation.validateNumeric(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessages.INVALID_ONLY_NUMERIC.getMessage());
     }
 
-    @DisplayName("입력값이 Null 또는 빈칸이 아니고 공백없이 숫자로만 이뤄경우 예외가 발생하지 않는다")
+    @DisplayName("입력값이 Null 또는 빈칸이 아니면 예외를 발생하지 않는다")
     @Test
-    void should_notThrowException_when_inputIsNumericWithoutWhitespace(){
+    void nonErrorNullOrEmpty() {
         // given
         String input = "1000";
 
         // when// then
-        assertThatCode(()->InputValidation.validate(input))
+        assertThatCode(()->InputValidation.validateNullOrEmpty(input))
                 .doesNotThrowAnyException();
     }
+
+    @DisplayName("입력값이 공백이 포함되지 않으면 예외를 발생하지 않는다")
+    @Test
+    void nonErrorBlank() {
+        // given
+        String input = "2000";
+
+        // when// then
+        assertThatCode(()->InputValidation.validateBlankContains(input))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("입력값이 정확한 정수값을 입력할 경우 예외를 발생핮지않는다")
+    @Test
+    void nonErrorNumerical() {
+        // given
+        String input = "3000";
+
+        // when// then
+        assertThatCode(()->InputValidation.validateNumeric(input))
+                .doesNotThrowAnyException();
+    }
+
+
 }
