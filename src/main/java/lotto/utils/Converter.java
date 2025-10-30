f1453d60bfb6e03f4023d435038d3c792b0201f3
A7024E <rudqls6671@naver.com>
2025-10-30 10:16:03 +0900

    feat: 정수로 이뤄지는 리스트 만드는 유틸성 메서드 추가

--- src/main/java/lotto/utils/Converter.java
+++ src/main/java/lotto/utils/Converter.java
@@ -1,7 +1,18 @@
 package lotto.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Converter {
     public static int toInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static List<Integer> toIntList(String[] inputs) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String input : inputs) {
+            numbers.add(toInt(input));
+        }
+        return numbers;
     }
 }
