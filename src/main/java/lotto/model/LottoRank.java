b55c4921771a60366a30cfe82aea6be1a4b0ec69
A7024E <rudqls6671@naver.com>
2025-10-31 03:56:27 +0900

    refactor: LottoRank 상수 순서 변경

--- src/main/java/lotto/model/LottoRank.java
+++ src/main/java/lotto/model/LottoRank.java
@@ -3,12 +3,12 @@ package lotto.model;
 import java.util.List;
 
 public enum LottoRank {
-    FIRST("6개 일치 (2,000,000,000원)", 2000000000L),
-    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000L),
-    THIRD("5개 일치 (1,500,000원)", 1500000L),
-    FOURTH("4개 일치 (50,000원)", 50000L),
-    FIFTH("3개 일치 (5,000원)", 5000L),
-    MISS("3개 미만 일치 (0원)", 0L);
+    MISS("3개 미만 일치 (0원)", 0L),
+    FIFTH("3개 일치 (5,000원)", 5_000L),
+    FOURTH("4개 일치 (50,000원)", 50_000L),
+    THIRD("5개 일치 (1,500,000원)", 1_500_000L),
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000L),
+    FIRST("6개 일치 (2,000,000,000원)", 2_000_000_000L);
 
     private final String description;
     private final long winningAmount;
