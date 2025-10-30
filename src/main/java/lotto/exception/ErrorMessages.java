f03e67a4e36ae4529bb30fcfe0375329fff4b674
A7024E <rudqls6671@naver.com>
2025-10-30 10:15:45 +0900

    feat: 로또 번호 에러 메시지 추가

--- src/main/java/lotto/exception/ErrorMessages.java
+++ src/main/java/lotto/exception/ErrorMessages.java
@@ -7,7 +7,10 @@ public enum ErrorMessages {
     INVALID_ONLY_NUMERIC("정수만 입력 가능합니다 다시 입력해 주세요."),
     INVALID_CONTAINS_WHITESPACE("공백을 입력할 수 없습니다. 공백을 제외하고 다시 입력해 주세요."),
     INVALID_NOT_DIVIDE_PURCHASE_AMOUNT("금액은 1000원 단위로 입력해야 합니다"),
-    INVALID_PURCHASE_AMOUNT_MINIMUM("시작 금액은 1000원 부터입니다.");
+    INVALID_PURCHASE_AMOUNT_MINIMUM("시작 금액은 1000원 부터입니다."),
+    INVALID_LOTTO_NUMBER_SIZE("로또 번호는 6개여야 합니다."),
+    INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45까지 입니다"),
+    INVALID_LOTTO_NUMBER_DUPLICATES("로또 번호는 중복될 수 없습니다");
 
     private final String message;
 
