66ca74f1459f5134225c402b2b28b17623ad5d15
A7024E <rudqls6671@naver.com>
2025-10-30 12:39:04 +0900

    feat(ErrorMessages): 당첨번호 예외사항 메시지 추가

--- src/main/java/lotto/exception/ErrorMessages.java
+++ src/main/java/lotto/exception/ErrorMessages.java
@@ -6,11 +6,14 @@ public enum ErrorMessages {
     INVALID_EMPTY_INPUT("빈 값을 입력할 수 없습니다. 다시 입력해 주세요."),
     INVALID_ONLY_NUMERIC("정수만 입력 가능합니다 다시 입력해 주세요."),
     INVALID_CONTAINS_WHITESPACE("공백을 입력할 수 없습니다. 공백을 제외하고 다시 입력해 주세요."),
-    INVALID_NOT_DIVIDE_PURCHASE_AMOUNT("금액은 1000원 단위로 입력해야 합니다"),
+    INVALID_NOT_DIVIDE_PURCHASE_AMOUNT("금액은 1000원 단위로 입력해야 합니다."),
     INVALID_PURCHASE_AMOUNT_MINIMUM("시작 금액은 1000원 부터입니다."),
     INVALID_LOTTO_NUMBER_SIZE("로또 번호는 6개여야 합니다."),
-    INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45까지 입니다"),
-    INVALID_LOTTO_NUMBER_DUPLICATES("로또 번호는 중복될 수 없습니다");
+    INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1부터 45까지 입니다."),
+    INVALID_LOTTO_NUMBER_DUPLICATES("로또 번호는 중복될 수 없습니다."),
+    INVALID_WINNING_NUMBER_DELIMITER("당첨 번호는 공백없이 쉼표로 구분된 정수들로 입력해야 합니다."),
+    INVALID_WINNING_NUMBER_RANGE("당첨 번호는 1부터 45까지만 입력 가능합니다."),
+    INVALID_WINNING_NUMBER_DUPLICATE("중복된 당첨 번호를 입력할수 없습니다.");
 
     private final String message;
 
